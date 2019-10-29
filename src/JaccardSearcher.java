5b00a927e50de7d4c49733cd5a2d256eb3e322db
Poafs1 <poafs1@hotmail.com>
2019-10-29 23:40:01 +0700

    update a few code in JaccardSearcher.java

--- src/JaccardSearcher.java
+++ src/JaccardSearcher.java
@@ -5,17 +5,22 @@
 import java.util.List;
 
 public class JaccardSearcher extends Searcher{
-
+	List<Document> parse;
 	public JaccardSearcher(String docFilename) {
 		super(docFilename);
 		/************* YOUR CODE HERE ******************/
-		
+		parse = Searcher.parseDocumentFromFile(docFilename);
 		/***********************************************/
 	}
 
 	@Override
 	public List<SearchResult> search(String queryString, int k) {
 		/************* YOUR CODE HERE ******************/
+		List<String> tokens = Searcher.tokenize(queryString);
+		for(int i=0; i<k; i++) {
+			if(tokens.size() == 0) break;
+		}
+
 		return null;
 		/***********************************************/
 	}
