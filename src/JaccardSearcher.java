0fd4b36a74af7e940a76807e3d93cad13a9ec673
Poafs1 <poafs1@hotmail.com>
2019-10-30 11:38:19 +0700

    Update Jaccard Searcher

--- src/JaccardSearcher.java
+++ src/JaccardSearcher.java
@@ -2,7 +2,11 @@
 //Section: 
 //ID: 
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Collections;
 
 public class JaccardSearcher extends Searcher{
 	List<Document> parse;
