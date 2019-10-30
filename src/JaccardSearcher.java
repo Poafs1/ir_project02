25c6a505bf48309b6ec272b0941d1f82b1e63095
Poafs1 <poafs1@hotmail.com>
2019-10-30 21:13:47 +0700

    Jaccard fixed bug

--- src/JaccardSearcher.java
+++ src/JaccardSearcher.java
@@ -9,11 +9,12 @@ import java.util.Set;
 import java.util.Collections;
 
 public class JaccardSearcher extends Searcher{
-	private List<Document> documents;
+	private List<Document> doc;
 	public JaccardSearcher(String docFilename) {
 		super(docFilename);
 		/************* YOUR CODE HERE ******************/
-		this.documents = Searcher.parseDocumentFromFile(docFilename);;
+//		this.documents = Searcher.parseDocumentFromFile(docFilename);
+		this.doc = this.documents;
 		/***********************************************/
 	}
 
@@ -25,13 +26,13 @@ public class JaccardSearcher extends Searcher{
 		List<SearchResult> allResults = new ArrayList<SearchResult>();
 		List<SearchResult> output = new ArrayList<SearchResult>();
 
-		for(int i=0; i<documents.size(); i++) {
-			Set<String> intersecDocument = new HashSet<String>(documents.get(i).getTokens());
-			Set<String> unionDocument = new HashSet<String>(documents.get(i).getTokens());
+		for(int i=0; i<doc.size(); i++) {
+			Set<String> intersecDocument = new HashSet<String>(doc.get(i).getTokens());
+			Set<String> unionDocument = new HashSet<String>(doc.get(i).getTokens());
 			intersecDocument.retainAll(queryTerms);
 			unionDocument.addAll(queryTerms);
 			double calculateScore = new Double(intersecDocument.size()) / new Double(unionDocument.size());
-			SearchResult getResult = new SearchResult(documents.get(i), calculateScore);
+			SearchResult getResult = new SearchResult(doc.get(i), calculateScore);
 			allResults.add(getResult);
 		}
 
